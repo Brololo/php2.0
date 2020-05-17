563ac958b67623f8b542f362cb695e3accb757ef
laurent.marechal@ynov.com <laurent.marechal@ynov.com>
2020-05-17 22:50:53 +0200

    encore plus de css

--- note/FaireOffre.php
+++ note/FaireOffre.php
@@ -1,23 +1,42 @@
 <?php
 
 session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="utf-8">
+        <title>TP noté</title>
+        <link rel="stylesheet" href="css/offre.css">
+    </head>
+    <body>
+
+
+
+<?php
 
 require('PDO.php');
 $pdo = bddConnect();
 
 echo "
-
-<form action='SendOffre.php' method='post'>
-    <label>Titre : <input type='text' name='titre'></label><br>
-    <label>Email : <input type='text' name='emailpro'></label><br>
-    <label>Téléphone : <input type='text' name='Téléphone'></label><br>
-    <label>Location : <input type='text' name='location'></label><br>
-    <label>Diplome minimun : <input type='text' name='diplome'></label><br>
-    <label>Décrire l'offre :
-    <textarea name='description'></textarea></label><br>
-    <button type='submit'>Envoyer</button>
-</form>
+<div id='premier'>
+    <form action='SendOffre.php' method='post'>
+        <label class='label'>Titre : <input type='text' name='titre'></label><br>
+        <label class='label'>Email : <input type='text' name='emailpro'></label><br>
+        <label class='label'>Téléphone : <input type='text' name='Téléphone'></label><br>
+        <label class='label'>Location : <input type='text' name='location'></label><br>
+        <label class='label'>Diplome minimun : <input type='text' name='diplome'></label><br>
+        <label class='label'>Décrire l'offre :
+        <textarea name='description'></textarea></label><br>
+        <button class='button' type='submit'>Envoyer</button>
+    </form>
+</div>
 
 "
 
 ?>
+
+    </body>
+</html>
